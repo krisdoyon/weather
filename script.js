765c1b266a8c13482b3df9f56a1a905ff603d0f7
Kris Doyon <kristopher.doyon@gmail.com>
2022-09-27 14:08:40 -0400

    Fix moon time formatting

--- script.js
+++ script.js
@@ -67,32 +67,99 @@ const cardSearch = document.querySelector(".card-search");
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // FUNCTIONS
 
+// API CALLS
+
+const getMeteoData = async function (lat, lon) {
+  const meteoResponse = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&temperature_unit=fahrenheit&daily=precipitation_sum,temperature_2m_max,temperature_2m_min,weathercode&timezone=auto`
+  );
+  if (!meteoResponse.ok)
+    throw new Error("Problem getting current weather data from OpenMeteo");
+  meteoData = await meteoResponse.json();
+  console.log(`METEO DATA---------------`);
+  console.log(meteoData);
+};
+
+const getOpenData = async function (lat, lon) {
+  const openResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2365b77577087dbfc79d7061d3559704&units=imperial`
+  );
+  if (!openResponse.ok)
+    throw new Error("Problem getting weather data from OpenWeatherMap");
+  openData = await openResponse.json();
+  console.log(`OPEN DATA---------------`);
+  console.log(openData);
+};
+
+const getMoonData = async function (lat, lon) {
+  const moonResponse = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}/${todayISO}/?key=G2KTKKSDX2CA79SJTJAYA52LU&include=days&elements=moonrise,moonset&timezone=${meteoData.timezone}`
+  );
+  console.log(todayISO);
+  if (!moonResponse.ok) throw new Error("Problem getting moon data");
+  moonData = await moonResponse.json();
+  console.log(`MOON DATA---------------`);
+  console.log(moonData);
+};
+
 const getWeatherData = async function (lat, lon) {
+  console.log("GETTING WEATHER DATA");
   try {
-    const meteoResponse = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&temperature_unit=fahrenheit&daily=precipitation_sum,temperature_2m_max,temperature_2m_min,weathercode&timezone=auto`
-    );
-    if (!meteoResponse.ok)
-      throw new Error("Problem getting current weather data from OpenMeteo");
-    meteoData = await meteoResponse.json();
-    const openResponse = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2365b77577087dbfc79d7061d3559704&units=imperial`
-    );
-    if (!openResponse.ok)
-      throw new Error("Problem getting weather data from OpenWeatherMap");
-    openData = await openResponse.json();
+    await Promise.all([getMeteoData(lat, lon), getOpenData(lat, lon)]);
     setDate();
     todayIndex = meteoData.daily.time.findIndex((date) => date === todayISO);
-    const moonResponse = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}/${todayISO}/?key=G2KTKKSDX2CA79SJTJAYA52LU&include=days&elements=moonrise,moonset&timezone=Z`
-    );
-    if (!moonResponse.ok) throw new Error("Problem getting moon data");
-    moonData = await moonResponse.json();
+    await getMoonData(lat, lon);
     displayWeather();
   } catch (err) {
     console.error(err);
   }
 };
+
+const getSearchResults = async function (query) {
+  const response = await fetch(
+    `https://nominatim.openstreetmap.org/search?format=json&q=${query}`
+  );
+  const data = await response.json();
+  return data;
+};
+
+// DATE AND LOCATION FUNCTIONS
+
+const setDate = function () {
+  const now = new Date();
+  todayISO = new Intl.DateTimeFormat("en-CA", {
+    timeZone: `${meteoData.timezone}`,
+  }).format(now);
+  const today = new Intl.DateTimeFormat(locale, {
+    timeZone: `${meteoData.timezone}`,
+    month: "long",
+    day: "2-digit",
+  }).format(now);
+  todayLabel.textContent = today;
+};
+
+const getCurrentPosition = async function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const handleLocationClick = async function () {
+  try {
+    const position = await getCurrentPosition();
+    const { latitude: lat, longitude: lon } = position.coords;
+    const response = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`
+    );
+    const data = await response.json();
+    labelLocation.textContent = data.display_name;
+    getWeatherData(lat, lon);
+  } catch (err) {
+    alert("Could not get your location.");
+  }
+};
+
+// DISPLAY FUNCTIONS
 
 const displayWeather = function () {
   displayCurrent();
@@ -112,28 +179,56 @@ const displayCurrent = function () {
       src="https://openweathermap.org/img/wn/${
         weatherCodeMap.get(meteoData.current_weather.weathercode).icon
       }@2x.png"
-    /><span class="current-conditions">${
-      weatherCodeMap.get(meteoData.current_weather.weathercode).label
-    }
+      /><span class="current-conditions">${
+        weatherCodeMap.get(meteoData.current_weather.weathercode).label
+      }
       </span>
-    <span class="temp-range">${Math.round(
-      meteoData.daily.temperature_2m_max[todayIndex]
-    )} / ${Math.round(
+      <span class="temp-range">${Math.round(
+        meteoData.daily.temperature_2m_max[todayIndex]
+      )} / ${Math.round(
     meteoData.daily.temperature_2m_min[todayIndex]
   )} &deg;F</span>
-  </div>
-  <div class="card-column">
-    <span class="current-temp"
+    </div>
+      <div class="card-column">
+      <span class="current-temp"
       >${Math.round(
         openData.main.temp
       )}<span class="current-temp-unit"> &deg;F</span></span
     ><span class="feels-like">Feels like: ${Math.round(
       openData.main.feels_like
     )} &deg;F</span>
-
   </div>`;
-
   cardCurrent.innerHTML = html;
+};
+
+const displayForecast = function () {
+  const forecastDates = meteoData.daily.time.slice(
+    todayIndex + 1,
+    todayIndex + 6
+  );
+  const htmlArr = forecastDates.map((date, i) => {
+    const html = `<div class="card-forecast__day">
+    <strong>${weekdays[new Date(`${date} 00:00:00`).getDay()]}</strong>
+    <span>${date.split("-").slice(1, 3).join("/")}</span>
+    <img
+      class="weather-icon"
+      src="https://openweathermap.org/img/wn/${
+        weatherCodeMap.get(meteoData.daily.weathercode[todayIndex + i + 1]).icon
+      }@2x.png"
+    />
+    <span>${
+      weatherCodeMap.get(meteoData.daily.weathercode[todayIndex + i + 1]).label
+    }</span>
+    <span>${Math.round(
+      meteoData.daily.temperature_2m_max[todayIndex + i + 1]
+    )} / ${Math.round(
+      meteoData.daily.temperature_2m_min[todayIndex + i + 1]
+    )} &deg;F</span>
+  </div>`;
+    return html;
+  });
+  cardForecast.innerHTML = "";
+  htmlArr.forEach((html) => cardForecast.insertAdjacentHTML("beforeend", html));
 };
 
 const displayDetails = function () {
@@ -178,147 +273,6 @@ const displayDetails = function () {
   cardDetails.innerHTML = html;
 };
 
-const displayForecast = function () {
-  const forecastDates = meteoData.daily.time.slice(
-    todayIndex + 1,
-    todayIndex + 6
-  );
-  const htmlArr = forecastDates.map((date, i) => {
-    const html = `<div class="card-forecast__day">
-    <strong>${weekdays[new Date(`${date} 00:00:00`).getDay()]}</strong>
-    <span>${date.split("-").slice(1, 3).join("/")}</span>
-    <img
-      class="weather-icon"
-      src="https://openweathermap.org/img/wn/${
-        weatherCodeMap.get(meteoData.daily.weathercode[todayIndex + i + 1]).icon
-      }@2x.png"
-    />
-    <span>${
-      weatherCodeMap.get(meteoData.daily.weathercode[todayIndex + i + 1]).label
-    }</span>
-    <span>${Math.round(
-      meteoData.daily.temperature_2m_max[todayIndex + i + 1]
-    )} / ${Math.round(
-      meteoData.daily.temperature_2m_min[todayIndex + i + 1]
-    )} &deg;F</span>
-  </div>`;
-    return html;
-  });
-  cardForecast.innerHTML = "";
-  htmlArr.forEach((html) => cardForecast.insertAdjacentHTML("beforeend", html));
-};
-
-const getSearchResults = async function (query) {
-  const response = await fetch(
-    `https://nominatim.openstreetmap.org/search?format=json&q=${query}`
-  );
-  const data = await response.json();
-  return data;
-};
-
-const displaySearchCard = function () {
-  cardSearch.style.maxHeight = "30rem";
-  cardSearch.style.visibility = "visible";
-  cardSearch.style.opacity = "1";
-};
-
-const hideSearchCard = function () {
-  cardSearch.innerHTML = "";
-  cardSearch.style.maxHeight = "0rem";
-  cardSearch.style.visibility = "hidden";
-  cardSearch.style.opacity = "0";
-};
-
-const renderSearchResults = function (query) {
-  cardSearch.innerHTML = "";
-
-  getSearchResults(query).then((data) => {
-    if (!data.length) {
-      const html = `<div class="no-results">No matching results. <br/> Please try again.</div>`;
-      cardSearch.insertAdjacentHTML("beforeend", html);
-      return;
-    }
-
-    data.forEach((result) => {
-      const html = `<button class="btn btn-search-result" data-lat="${result.lat}" data-lon="${result.lon}" data-label="${result.display_name}">
-        ${result.display_name}</button>
-      `;
-      cardSearch.insertAdjacentHTML("beforeend", html);
-    });
-  });
-};
-
-const getCurrentPosition = async function () {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
-};
-
-const handleLocationClick = async function () {
-  try {
-    const position = await getCurrentPosition();
-    const { latitude: lat, longitude: lon } = position.coords;
-    const response = await fetch(
-      `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`
-    );
-    const data = await response.json();
-    labelLocation.textContent = data.display_name;
-    getWeatherData(lat, lon);
-  } catch (err) {
-    alert("Could not get your location.");
-  }
-};
-
-const setDate = function () {
-  const now = new Date();
-  todayISO = new Intl.DateTimeFormat("en-CA", {
-    timeZone: `${meteoData.timezone}`,
-  }).format(now);
-  const today = new Intl.DateTimeFormat(locale, {
-    timeZone: `${meteoData.timezone}`,
-    month: "long",
-    day: "2-digit",
-  }).format(now);
-  todayLabel.textContent = today;
-};
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
-// EVENT HANDLERS
-
-document.addEventListener("click", function (e) {
-  !e.target.classList.contains("card-search") &&
-    !e.target.closest(".container-search") &&
-    hideSearchCard();
-});
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Enter" && document.activeElement === inputSearch) {
-    btnSearch.click();
-  }
-});
-
-containerSearch.addEventListener("click", function (e) {
-  if (e.target.closest(".btn-location")) {
-    handleLocationClick();
-    hideSearchCard();
-  } else if (e.target.classList.contains("btn-search")) {
-    inputSearch.value || alert("Please enter a search term");
-    if (!inputSearch.value) return;
-    renderSearchResults(inputSearch.value);
-    displaySearchCard();
-    isSearchOpen = true;
-  } else return;
-});
-
-cardSearch.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".btn-search-result");
-  if (!clicked) return;
-  getWeatherData(clicked.dataset.lat, clicked.dataset.lon);
-  labelLocation.textContent = clicked.dataset.label;
-  hideSearchCard();
-  inputSearch.value = "";
-});
-
 const displayAst = function () {
   const dayLength = (openData.sys.sunset - openData.sys.sunrise) / 60 / 60;
   const html = `
@@ -357,28 +311,89 @@ const displayAst = function () {
       ).format(new Date(openData.sys.sunset * 1000))}</span>
     </div>
     <div class="flex-column">
-      <span class="ast-info"><strong>Rise: </strong>${new Intl.DateTimeFormat(
-        locale,
-        {
-          timeZone: `${meteoData.timezone}`,
-          hour: "numeric",
-          minute: "numeric",
-          hour12: true,
-        }
-      ).format(
-        new Date(todayISO + `T` + moonData.days[0].moonrise + "Z")
+      <span class="ast-info"><strong>Rise: </strong>${convertTo12Hr(
+        moonData.days[0].moonrise
       )}</span>
-      <span class="ast-info"><strong>Set: </strong>${new Intl.DateTimeFormat(
-        locale,
-        {
-          timeZone: `${meteoData.timezone}`,
-          hour: "numeric",
-          minute: "numeric",
-          hour12: true,
-        }
-      ).format(
-        new Date(todayISO + `T` + moonData.days[0].moonset + "Z")
+      <span class="ast-info"><strong>Set: </strong>${convertTo12Hr(
+        moonData.days[0].moonset
       )}</span>
     </div>`;
   cardAst.innerHTML = html;
 };
+
+const displaySearchCard = function () {
+  cardSearch.style.height = "25rem";
+  cardSearch.style.visibility = "visible";
+  cardSearch.style.opacity = "1";
+};
+
+const hideSearchCard = function () {
+  cardSearch.innerHTML = "";
+  cardSearch.style.height = "0rem";
+  cardSearch.style.visibility = "hidden";
+  cardSearch.style.opacity = "0";
+};
+
+const renderSearchResults = function (query) {
+  cardSearch.innerHTML = "";
+
+  getSearchResults(query).then((data) => {
+    if (!data.length) {
+      const html = `<div class="no-results">No matching results. <br/> Please try again.</div>`;
+      cardSearch.insertAdjacentHTML("beforeend", html);
+      return;
+    }
+
+    data.forEach((result) => {
+      const html = `<button class="btn btn-search-result" data-lat="${result.lat}" data-lon="${result.lon}" data-label="${result.display_name}">
+        ${result.display_name}</button>
+      `;
+      cardSearch.insertAdjacentHTML("beforeend", html);
+    });
+  });
+};
+
+const convertTo12Hr = function (time) {
+  const timeArr = time.slice(0, -3).split(":");
+  let [hour, min] = timeArr;
+  const suffix = hour > 12 ? "PM" : "AM";
+  hour = hour > 12 ? hour - 12 : hour;
+  return `${hour}:${min} ${suffix}`;
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// EVENT HANDLERS
+
+document.addEventListener("click", function (e) {
+  !e.target.classList.contains("card-search") &&
+    !e.target.closest(".container-search") &&
+    hideSearchCard();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    btnSearch.click();
+  }
+});
+
+containerSearch.addEventListener("click", function (e) {
+  if (e.target.closest(".btn-location")) {
+    handleLocationClick();
+    hideSearchCard();
+  } else if (e.target.classList.contains("btn-search")) {
+    inputSearch.value || alert("Please enter a search term");
+    if (!inputSearch.value) return;
+    renderSearchResults(inputSearch.value);
+    displaySearchCard();
+    isSearchOpen = true;
+  } else return;
+});
+
+cardSearch.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".btn-search-result");
+  if (!clicked) return;
+  getWeatherData(clicked.dataset.lat, clicked.dataset.lon);
+  labelLocation.textContent = clicked.dataset.label;
+  hideSearchCard();
+  inputSearch.value = "";
+});
